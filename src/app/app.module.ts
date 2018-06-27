4a6bbf35d695344748e2e70e5a0f44553cb72100
Guido <guidoperman@gmail.com>
2018-06-27 14:51:00 -0300

    Capitalizado listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { LOCALE_ID } from "@angular/core";
+
+import { CapitalizadoPipe } from "./pipes/capitalizado.pipe";
 
 import { AppComponent } from './app.component';
-
-
+//{ provide: LOCALE_ID, useValue: "es" }
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
